9dbd8fc79e460c1402ac42aedee4638105e404a6
Surfsol <surfsol@protonamil.com>
2019-11-21 01:21:00 -0300

    Add RedDisplay

--- src/actions/redActions.js
+++ src/actions/redActions.js
@@ -25,3 +25,24 @@ export const fetchRed= (red) => dispatch => {
       })
       .catch(err =>  dispatch({ type: RED_FAILURE, payload: err }));
 };
+
+export const FAV_FETCH = 'FAV_FETCH';
+export const FAV_SUCCESS = 'FAV_SUCCESS';
+export const FAV_FAILURE = 'FAV_FAILURE';
+
+// action creator
+//fetchLog imports to Login
+export const favRed= (red) => dispatch => {
+    console.log(`red`, red)
+  // action objects
+    //dispatch({ type: RED_FETCH})
+  // from thunk (see below) do some async action and dispatch an error or success action
+  axiosWithAuth()
+      .post('https://davidanagy-posthere-flask.herokuapp.com/subreddit', red )
+      .then(res => {
+         console.log(res.data)
+        dispatch({ type: RED_SUCCESS, payload: res.data})
+
+      })
+      .catch(err =>  dispatch({ type: RED_FAILURE, payload: err }));
+};
--- src/components/RedDisplay.js
+++ src/components/RedDisplay.js
@@ -38,50 +38,8 @@ const useStyles = makeStyles({
     return (
       <>
       <div>
-        <NAVBAR />
-        <Card>
-          <CardBody>
-            <CardTitle>Business Name: {props.fact.business_name} </CardTitle>
-            <CardSubtitle>Business Address: {props.fact.address} </CardSubtitle>
-            <CardText>Business ID: {props.fact.id}</CardText>
-            </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                {/* <CardLink className="stars" href="#"><AdjustedRating /></CardLink> */}
-          </CardBody>
-        </Card>
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Typography component="legend"><h1>Rating:</h1></Typography>
-          <div className="stars">
-            <Rating
-              name="hover-side"
-              value={props.fact.user_review}
-              readOnly = {true}
-            />
-          </div>
-        </Box>
-        <div>
-                <Card>
-                <CardText className="text-review">Business Review:
-                  <br></br>
-                   {props.fact.original_text_review}</CardText>
-                </Card>
-        </div>
-  
-        {/* adjusted stars */}
-        <div id="myDIV">
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Typography component="legend"><h1>Adjusted Rating:</h1></Typography>
-          <div className="stars">
-            <Rating
-              name="hover-side"
-              value={props.fact.yelp_rating}
-              readOnly = {true}
-            />
-          </div>
-        </Box>
-        </div>
+    <h3>Title: {props.red.title}</h3>
+    <h3>Content: {props.red.text}</h3>
       </div>
       </>
     );
